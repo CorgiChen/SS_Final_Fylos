1e0fa1f27529ed8d9ead994d651bce7ec9dc9afa
Qmaochen <qmaochen0917@gmail.com>
2025-06-05 18:11:03 +0800

    chore: update .gitignore to exclude Cocos Creator temporary files

--- temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,9 +0,0 @@
-
-require('./assets/Script/Camera');
-require('./assets/Script/CameraController');
-require('./assets/Script/ChatBubbleController');
-require('./assets/Script/Player');
-require('./assets/Script/PressStart');
-require('./assets/Script/Transport');
-require('./assets/Script/VideoPlayerController');
-require('./assets/Script/Wind');
